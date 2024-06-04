81130c344dcfd152295bd273e193e368d004d04a
ferizoozoo <farhadzohoorz@gmail.com>
2024-06-04 10:06:13 +0330

    solved: quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples: u32) -> u32 {
+    let mut price: u32 = 0;
+    if apples > 40 {
+        price = apples;
+    } else {
+        price = apples * 2;
+    }
+
+    return price;
+}
 
 // Don't modify this function!
 #[test]
